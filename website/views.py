3d3970c4ce5c2c06b9da3a031931550931c63662
krishna <19pa1a04h0@vishnu.edu.in>
2022-12-26 11:32:12 +0530

    Initial commit

--- website/views.py
+++ website/views.py
@@ -16,7 +16,7 @@ def get(request):
   
   product = request.GET["product"]
 
-  
+  print(product)
   amazon_url_1="https://www.amazon.in/s?k="
   amazon_url_2="&crid=30H5OAIPN2OL9&sprefix=%2Caps%2C233&ref=nb_sb_ss_recent_1_0_recent"
 
@@ -24,9 +24,9 @@ def get(request):
   page=requests.get(url,headers=headers)
   data=BeautifulSoup(page.text,"lxml")
   data.prettify()
-  
+  final=data.find_all(class_="s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16")
 
-
+ 
   title=data.find_all(class_=["a-size-medium a-color-base a-text-normal"])
   price=data.find_all(class_="a-price-whole")
   image_link=data.find_all(class_="s-image")
@@ -39,9 +39,17 @@ def get(request):
     item = Product()
     
     item.title=title[i].text
-    item.image=image_link[i]["src"]
+    
+    
     item.price=price[i].text
-    item.link="http://amazon.in"+product_link[i]['href']
+    try:
+      item.link="http://amazon.in"+product_link[i]['href']
+    except:
+      continue
+    try:
+      item.image=image_link[i]["src"]
+    except:
+      continue
     amazon_product_list.append(item)
 
   flipkart_url1='https://www.flipkart.com/search?q='
@@ -56,7 +64,7 @@ def get(request):
 
   title=data.find_all(class_="_4rR01T")
 
-  image_link=data.find_all(class_="_396cs4 _3exPp9")
+  image_link=data.find_all(class_="_396cs4")
 
   price=data.find_all(class_="_30jeq3 _1_WHN1")
 
@@ -65,7 +73,7 @@ def get(request):
   if(len(title)==0):
 
     title=data.find_all(class_="s1Q9rs")
-    image_link=data.find_all(class_="_396cs4 _3exPp9")
+    image_link=data.find_all(class_="_396cs4")
     price=data.find_all(class_="_30jeq3")
     product_link = data.find_all(class_="_2rpwqI")
   if len(title) == 0:
@@ -75,11 +83,14 @@ def get(request):
     product_link = data.find_all(class_="_3bPFwb")
 
   flipkart_product_list = []
-
-  for i in range(1,len(image_link)):
+  x=min(len(title),len(image_link),len(price),len(product_link))
+  for i in range(1,x):
     item = Product()
 
     item.title=title[i].text
+    
+  
+
     item.image=image_link[i]["src"]
     item.price=price[i].text
     item.link="http://flipkart.com"+product_link[i]['href']
